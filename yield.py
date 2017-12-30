7864408f38809a4b8514364e9b681d9666901725
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-12-30 14:18:36 +0300

    Update yield.py

--- yield.py
+++ yield.py
@@ -1,11 +1,10 @@
-
 import os
 def fflist(path):
 	dir = [path]
 	for j in dir:
+		yield [j]
 		if os.path.isdir(j) is True:
 			for k in os.listdir(j):
 				dir.extend([j+'/'+k])
-				yield [j+'/'+k]
 for test in fflist('/home/linux/Test'):
 	print test
